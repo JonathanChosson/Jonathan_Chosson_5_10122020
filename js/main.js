5b92c24f6692e15b6590ae0e7727be43aaabfd21
Joanthan Chosson <catstrike@gmail.com>
2020-12-11 13:14:15 +0100

    [Build]index.html :construction: mise en place des produits

--- js/main.js
+++ js/main.js
@@ -1,21 +1,40 @@
-    let url = `http://127.0.0.1:3000/api/cameras`;
-    fetch(url)
-    .then((reponse) => 
-    reponse.json()
-    .then((data) => {
-        console.log(data);
-    })
-    ).catch(erreur => console.log('erreur : ' + erreur));
+let url = `http://127.0.0.1:3000/api/cameras`;
+fetch(url)
+.then((reponse) => 
+reponse.json()
+.then((data) => {
+    console.log(data);
+    for(let tableauProduit of data){
+        console.log(tableauProduit);
+        let liste = document.getElementById('liste');
+        liste.innerHTML += `
+        <div class="col-sm-12 col-md-6 col-lg-3">
+            <div class="card">
+                <img src="${tableauProduit.imageUrl}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${tableauProduit.name}</h5>
+                        <p class="card-text">${tableauProduit.description}</p>
+                        <a href="#" class="btn btn-primary">Voir ce produit</a>
+                    </div>
+            </div>
+        </div>`;
+    }
+})
+).catch(erreur => console.log('erreur : ' + erreur));
 
 
 
 
-// let url = `http://127.0.0.1:3000/api/cameras`;
-//     fetch(url)
-//     .then((reponse) => 
-//     reponse.json()
-//     .then((data) => {
-//         console.log(data);
-        
-//     })
-//     ).catch(erreur => console.log('erreur : ' + erreur));
+
+//Afficher le toast
+// let toast = document.getElementById('myToast');
+// toast.classList.add("show");
+
+// console.log(document.getElementById('closeToast'));
+// document.getElementById('closeToast').addEventListener('click', function(){
+//     toast.classList.toggle("show");
+// })
+
+//recuperer donnée dans l'URL
+// let req =location.search.substring(1);
+// console.log(req);
