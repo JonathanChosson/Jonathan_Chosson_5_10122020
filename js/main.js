887291bc0744b6bac52728d0f1f08cefcc0c55ba
Jonathan Chosson <catstrike@gmail.com>
2020-12-16 11:35:02 +0100

    [JS] main.js :construction_worker: Modification nom variables

--- js/main.js
+++ js/main.js
@@ -3,24 +3,28 @@ fetch(url)
 .then((reponse) => 
 reponse.json()
 .then((data) => {
-    for(let tableauProduit of data){
+    for(let produit of data){
         let liste = document.getElementById('liste');
-        let prix = affichePrix(tableauProduit.price);
+        let prix = affichePrix(produit.price);
+        console.log(produit);
+        // class FicheProduit {
+        //     constructor
+        // }
         liste.innerHTML += `
         <div class="col-sm-12 col-md-6 col-lg-6 pb-3">
             <div class="card">
-                <img src="${tableauProduit.imageUrl}" class="card-img-top" alt="...">
+                <img src="${produit.imageUrl}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <div class="row">
                             <div class="col-7">
-                                <h5 class="card-title">${tableauProduit.name}</h5>
+                                <h5 class="card-title">${produit.name}</h5>
                             </div>
                             <div class="col-5 text-right">
                                 <h5 class="card-title">${prix} €</h5>
                             </div>
                         </div>
-                        <p class="card-text text-truncate">${tableauProduit.description}</p>
-                        <a href="./produit.html?_id=${tableauProduit._id}" class="btn btn-primary">Voir ce produit</a>
+                        <p class="card-text text-truncate">${produit.description}</p>
+                        <a href="./produit.html?_id=${produit._id}" class="btn btn-primary">Voir ce produit</a>
                     </div>
             </div>
         </div>`;
